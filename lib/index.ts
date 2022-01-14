975268e26e68bc8267dc5dc386831786edd5dde6
Adam K Dean <adamkdean@googlemail.com>
2022-01-14 13:40:46 +0000

    add typescript structure

---
+++ lib/index.ts
@@ -0,0 +1,6 @@
+// Copyright 2022 Adam K Dean <adamkdean@googlemail.com>
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+// TODO: library
